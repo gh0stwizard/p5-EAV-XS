cf356e8862aa0a781c7daf7cd245ab1f53d1943a
gh0stwizard <vitaliy.tokarev@gmail.com>
2017-04-22 05:43:47 +0300

    update license information

--- lib/EAV/XS.pm
+++ lib/EAV/XS.pm
@@ -3,7 +3,7 @@ package EAV::XS;
 use strict;
 use warnings;
 
-our $VERSION = eval '0.1.0';
+our $VERSION = eval '0.1.1';
 
 require XSLoader;
 XSLoader::load('EAV::XS', $VERSION);
@@ -137,9 +137,30 @@ Vitaliy V. Tokarev, E<lt>vitaliy.tokarev@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2017 by Vitaliy V. Tokarev
+Copyright (c) 2017 Vitaliy V. Tokarev
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 =cut
